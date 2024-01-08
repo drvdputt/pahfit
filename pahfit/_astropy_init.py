2b93be91fca4dac7ac1e80f429178ea402a0c613
Dries Van De Putte <dvandeputte@stsci.edu>
2024-01-08 17:49:32 -0500

    Quick fix for astropy 6.0.0 issue

--- pahfit/_astropy_init.py
+++ pahfit/_astropy_init.py
@@ -20,7 +20,7 @@ if not _ASTROPY_SETUP_:  # noqa
     import os
     from warnings import warn
     from astropy.config.configuration import (
-        update_default_config,
+        # update_default_config, # removed in astropy 6.0.0
         ConfigurationDefaultMissingError,
         ConfigurationDefaultMissingWarning,
     )
@@ -38,18 +38,18 @@ if not _ASTROPY_SETUP_:  # noqa
     if not os.environ.get("ASTROPY_SKIP_CONFIG_UPDATE", False):
         config_dir = os.path.dirname(__file__)
         config_template = os.path.join(config_dir, __package__ + ".cfg")
-        if os.path.isfile(config_template):
-            try:
-                update_default_config(__package__, config_dir, version=__version__)
-            except TypeError as orig_error:
-                try:
-                    update_default_config(__package__, config_dir)
-                except ConfigurationDefaultMissingError as e:
-                    wmsg = (
-                        e.args[0] + " Cannot install default profile. If you are "
-                        "importing from source, this is expected."
-                    )
-                    warn(ConfigurationDefaultMissingWarning(wmsg))
-                    del e
-                except Exception:
-                    raise orig_error
+        # if os.path.isfile(config_template):
+        #     # try:
+        #         # update_default_config(__package__, config_dir, version=__version__)
+        #     # except TypeError as orig_error:
+        #         # try:
+        #             # update_default_config(__package__, config_dir)
+        #         except ConfigurationDefaultMissingError as e:
+        #             wmsg = (
+        #                 e.args[0] + " Cannot install default profile. If you are "
+        #                 "importing from source, this is expected."
+        #             )
+        #             warn(ConfigurationDefaultMissingWarning(wmsg))
+        #             del e
+        #         except Exception:
+        #             raise orig_error
