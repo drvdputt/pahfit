7fff409b8cee3feef8d18a8f8fc139ba706f75aa
Dries Van De Putte <dvandeputte@stsci.edu>
2024-01-30 14:04:06 -0500

    Alternate pretty print implementation with ad-hoc string replacement

--- pahfit/features/features_format.py
+++ pahfit/features/features_format.py
@@ -2,18 +2,20 @@ import numpy.ma as ma
 from astropy.table import MaskedColumn
 from astropy.table.pprint import TableFormatter
 
+
 # * Special table formatting for bounded (val, min, max) values
 def fmt_func(fmt):
     def _fmt(v):
-        try: 
-            if ma.is_masked(v) or ma.is_masked(v[0]):
-                return "  <n/a>  "
+        try:
+            if ma.is_masked(v[0]):
+                return "  masked  "
             if ma.is_masked(v[1]):
                 return f"{v[0]:{fmt}} (Fixed)"
             return f"{v[0]:{fmt}} ({v[1]:{fmt}}, {v[2]:{fmt}})"
-        except IndexError as e:
-            print("bad value was passed to _fmt")
-            raise e
+        except Exception as e:
+            # print(f"problem in _fmt: v = {v}, fmt = {fmt}, type(fmt) = {type(fmt)}")
+            # raise e
+            return f"{v:0.4g}"
 
     return _fmt
 
@@ -27,8 +29,8 @@ class BoundedMaskedColumn(MaskedColumn):
 
     @property
     def shape(self):
-        sh = super().shape
-        return sh[0:-1] if self._omit_shape and len(sh) > 1 else sh
+        # return (sh[0],) if self._omit_shape and len(sh) > 1 else sh
+        return super().shape
 
     def is_fixed(self):
         return ma.getmask(self)[:, 1:].all(1)
@@ -40,17 +42,56 @@ class BoundedParTableFormatter(TableFormatter):
     'var', 'min', and 'max'.  To be set as Table's `TableFormatter'.
     """
 
-    def _pformat_table(self, table, *args, **kwargs):
-        bpcols = []
-        try:
-            colsh = [(col, col.shape) for col in table.columns.values()]
-            BoundedMaskedColumn._omit_shape = True
-            for col, sh in colsh:
-                if len(sh) == 2 and sh[1] == 3:
-                    bpcols.append((col, col.info.format))
-                    col.info.format = fmt_func(col.info.format or "g")
-            return super()._pformat_table(table, *args, **kwargs)
-        finally:
-            BoundedMaskedColumn._omit_shape = False
-            for col, fmt in bpcols:
-                col.info.format = fmt
+    # def _pformat_table(self, table, *args, **kwargs):
+    #     #return super()._pformat_table(table, *args, **kwargs)
+    #     bpcols = []
+    #     try:
+    #         colsh = [(col, col.shape) for col in table.columns.values()]
+    #         for col, sh in colsh:
+    #             if len(sh) == 2 and sh[1] == 3:
+    #                 bpcols.append((col, col.info.format))
+    #                 print("before", col.name, sh, col.info.format)
+    #                 col.info.format = fmt_func("0.4g")
+    #                 print("after", col.name, sh, col.info.format)
+    #         BoundedMaskedColumn._omit_shape = True
+    #         return super()._pformat_table(table, *args, **kwargs)
+    #     finally:
+    #         BoundedMaskedColumn._omit_shape = False
+    #         for col, fmt in bpcols:
+    #             print("finally", col.name, fmt)
+    #             col.info.format = fmt
+
+    def _pformat_col(
+        self,
+        col,
+        max_lines=None,
+        **kwargs,
+    ):
+        default_strings, outs = super()._pformat_col(col, max_lines, **kwargs)
+        has_bounds = len(col.shape) == 2 and col.shape[1] == 3
+        if not has_bounds:
+            strings = default_strings
+        else:
+            # copy the header strings
+            strings = default_strings[: outs["n_header"]]
+
+            # modify output of the numerical lines
+            # data index = line index - n_header
+            for i, line in enumerate(default_strings[outs["n_header"] :]):
+                if "..." in line or "table" in line:
+                    # generic ellipsis line
+                    strings.append(line)
+                elif "-- .. --" in line:
+                    # all masked
+                    strings.append(line.replace("-- .. --", "--"))
+                elif ".. --" in line:
+                    # value but masked bound
+                    strings.append(line.replace(".. --", "(fixed)"))
+                else:
+                    # all values
+                    strings.append(f"{col[i, 0]} ({col[i, 1]}, {col[i, 2]})")
+
+                # print("before:", repr(line))
+                # print("after:", repr(strings[-1]))
+
+        return strings, outs
